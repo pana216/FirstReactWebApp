36f47b8eb11ca1c82b551d8cc7fcbe67c7d9d35a
Panagiotis Diakas <d.panagiotis@hotmail.de>
2020-11-18 23:01:53 +0100

    MyApp Update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My First ReactApp</h1>
-        <p>Hello World</p>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+
+class App extends Component {
+  state= { 
+    ninjas : [
+      {name : "Kevin", age : "24", belt : "Red", id : 1},
+      {name : "Sahin", age : "24", belt : "White", id : 2},
+      {name : "Panagiotis", age : "23", belt : "Black2", id : 3}
+    ]
 }
 
+  render () {
+    return (
+      <div className="App">
+          <h1>My First ReactApp</h1>
+          <p>Hello World</p>
+          <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
+}
 export default App;
